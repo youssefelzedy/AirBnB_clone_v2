0a1eea379a371f1b271e4b3a0a0c6cbf5b96f128
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-04-02 04:14:49 +0200

    final

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Flask web application"""
+
+from flask import Flask, render_template
+
+skill_app = Flask(__name__)
+
+@skill_app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB message"""
+    return "Hello HBNB!"
+
+@skill_app.route('/hbnb', strict_slashes=False)
+def hello():
+    """HBNB message"""
+    return "HBNB!"
+
+if __name__ == "__main__":
+    skill_app.run(port=5000, host='0.0.0.0')
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Flask web application"""
+
+from flask import Flask, render_template
+
+skill_app = Flask(__name__)
+
+@skill_app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB message"""
+    return "Hello HBNB!"
+
+@skill_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB message"""
+    return "HBNB!"
+
+@skill_app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """text message"""
+    return "C {}".format(text.replace("_", " "))
+
+@skill_app.route('/python/<text>', strict_slashes=False)
+@skill_app.route('/python/', strict_slashes=False)
+def python(text="is cool"):
+    """python message"""
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    skill_app.run(port=5000, host='0.0.0.0')
